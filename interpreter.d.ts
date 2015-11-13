67e36db18969f2914728b5ebaa0b6beb52eed204
Stéphane Duguay <s@binarez.com>
2015-11-13 04:13:35 -0500

    [Typescript] OBJECT and getProperty now exposed.
    ^^

--- interpreter.d.ts
+++ interpreter.d.ts
@@ -2,9 +2,12 @@ declare class Interpreter
 {
 	constructor(codeText : string, initFunc : Function);
 
+	OBJECT : Object;
+
 	public step() : void;
 	public createPrimitive(data : any) : Object;
 	public createNativeFunction(func : any) : Object;
 	public createObject(parent : Object) : Object;
 	public setProperty( obj : Object, name : any, value : any, opt_fixed? : boolean, opt_nonenum? : boolean) : void;
+	public getProperty( obj : Object, name : string ) : Object;
 }
