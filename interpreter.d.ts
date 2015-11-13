a31516b94f52a0f58b0ff1f8640a85b36b2189db
Stéphane Duguay <s@binarez.com>
2015-11-13 01:02:58 -0500

    Adding interpreter.d.ts file
    Typescript declaration for the JSInterpreter class.

---
+++ interpreter.d.ts
@@ -0,0 +1,10 @@
+declare class JSInterpreter
+{
+	constructor(codeText : string, initFunc : Function);
+
+	public step() : void;
+	public createPrimitive(data : any) : Object;
+	public createNativeFunction(func : any) : Object;
+	public createObject(parent : Object) : Object;
+	public setProperty( obj : Object, name : any, value : any, opt_fixed? : boolean, opt_nonenum? : boolean) : void;
+}
