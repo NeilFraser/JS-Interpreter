162ef403a5e3c702b9c8694e51936ea651fd57bd
Neil Fraser <fraser@google.com>
2016-05-06 02:59:20 -0700

    Fix multiple errors with Array.indexOf/lastIndexOf

--- interpreter.js
+++ interpreter.js
@@ -390,6 +390,13 @@ Interpreter.prototype.initArray = function(scope) {
     }
     return n;
   };
+  var strictComp = function(a, b) {
+    // Strict === comparison.
+    if (a.isPrimitive && b.isPrimitive) {
+      return a.data === b.data;
+    }
+    return a === b;
+  };
   var wrapper;
   // Array constructor.
   wrapper = function(var_args) {
@@ -585,10 +592,10 @@ Interpreter.prototype.initArray = function(scope) {
     if (fromIndex < 0) {
       fromIndex = this.length + fromIndex;
     }
-    fromIndex = Math.max(0, Math.min(fromIndex, this.length));
+    fromIndex = Math.max(0, fromIndex);
     for (var i = fromIndex; i < this.length; i++) {
       var element = thisInterpreter.getProperty(this, i);
-      if (thisInterpreter.comp(element, searchElement) == 0) {
+      if (strictComp(element, searchElement)) {
         return thisInterpreter.createPrimitive(i);
       }
     }
@@ -603,10 +610,10 @@ Interpreter.prototype.initArray = function(scope) {
     if (fromIndex < 0) {
       fromIndex = this.length + fromIndex;
     }
-    fromIndex = Math.max(0, Math.min(fromIndex, this.length));
+    fromIndex = Math.min(fromIndex, this.length - 1);
     for (var i = fromIndex; i >= 0; i--) {
       var element = thisInterpreter.getProperty(this, i);
-      if (thisInterpreter.comp(element, searchElement) == 0) {
+      if (strictComp(element, searchElement)) {
         return thisInterpreter.createPrimitive(i);
       }
     }
