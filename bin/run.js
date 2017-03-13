223d3ecadc81bec922a6d1928eb15ad8100c4ba7
Paul Carduner <paul@carduner.net>
2017-03-13 18:23:55 +0000

    Add better documentation for run script

--- bin/run.js
+++ bin/run.js
@@ -1,18 +1,36 @@
 #!/usr/bin/env node
+/**
+ * This script is a command line runner for JS-Interpreter.
+ * It exposes a few "native" apis that can be used in the
+ * script being interpreted:
+ *
+ * 1. console.log(...args: string[]) - just a pass through to
+ *        node's native console.log implementation
+ *
+ * 2. vm.runInContext(source: string, context: Object) - executes
+ *        the interpreter against the given source, using the context
+ *        object for the global scope.
+ *
+ * Usage:
+ *   js-interpreter <filepath.js>
+ */
 
 window = typeof window === 'undefined' ? global : window;
 var fs = require('fs');
+const yargs = require('yargs');
+
 var Interpreter = require('../interpreter');
 
-// add native console object to global interpreter scope
-function initConsole(interpreter, scope) {
-  var myConsole = interpreter.createObject(interpreter.OBJECT);
-  interpreter.setProperty(
-    scope,
-    'console',
-    myConsole
-  );
+const argv = yargs
+  .usage(`Usage: $0 <filepath.js>`)
+  .help('h')
+  .alias('h', 'help')
+  .demandCommand(1)
+  .argv;
 
+
+function createConsoleObject(interpreter) {
+  var myConsole = interpreter.createObject(interpreter.OBJECT);
   // add log function to console object
   function myLog() {
     for (var j = 0; j < arguments.length; j++) {
@@ -26,19 +44,14 @@ function initConsole(interpreter, scope) {
     interpreter.createNativeFunction(myLog),
     Interpreter.NONENUMERABLE_DESCRIPTOR
   );
+  return myConsole;
+}
 
-
-  // add vm object
-  var myVM = interpreter.createObject(interpreter.OBJECT);
-  interpreter.setProperty(
-    scope,
-    'vm',
-    myVM
-  );
-
+function createVMObject(interpreter) {
+  var vm = interpreter.createObject(interpreter.OBJECT);
   function runInContext(source, context) {
     const interp = new Interpreter(source.toString(), function(interpreter, scope) {
-      initConsole(interpreter, scope);
+      initInterpreterScope(interpreter, scope);
       for (var key in context.properties) {
         interpreter.setProperty(scope, key, context.properties[key]);
       }
@@ -47,13 +60,32 @@ function initConsole(interpreter, scope) {
   }
 
   interpreter.setProperty(
-    myVM,
+    vm,
     'runInContext',
     interpreter.createNativeFunction(runInContext),
     Interpreter.NONENUMERABLE_DESCRIPTOR
   );
+  return vm;
+}
+
+// adds "native" global properties to the interpreter's scope
+function initInterpreterScope(interpreter, scope) {
+
+  // add native console object to global interpreter scope
+  interpreter.setProperty(
+    scope,
+    'console',
+    createConsoleObject(interpreter)
+  );
+
+  // add vm object
+  interpreter.setProperty(
+    scope,
+    'vm',
+    createVMObject(interpreter)
+  );
 
 }
 
-var code = fs.readFileSync(process.argv[process.argv.length - 1], 'utf-8');
-new Interpreter(code, initConsole).run();
+var code = fs.readFileSync(argv._[0], 'utf-8');
+new Interpreter(code, initInterpreterScope).run();
