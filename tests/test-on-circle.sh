456c1891fca7162b40ab449335776f1a44c59997
Paul Carduner <paul@carduner.net>
2017-03-21 11:46:59 -0700

    make sure we exit with the same exit code

--- tests/test-on-circle.sh
+++ tests/test-on-circle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
 tests/build-test-report.js -rd --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX
-
+t1=$?
 mv tests/test-results-new.json $CIRCLE_ARTIFACTS
+exit $t1
