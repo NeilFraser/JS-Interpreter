7e9cd7d735c7a9ed615c56d2adeac23f3a1077bb
Paul Carduner <paul@carduner.net>
2017-03-21 10:53:44 -0700

    Don't render progress on circle

--- tests/test-on-circle.sh
+++ tests/test-on-circle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
-tests/build-test-report.js -rd --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX --progress
+tests/build-test-report.js -rd --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX
 
 mv tests/test-results-new.json $CIRCLE_ARTIFACTS
