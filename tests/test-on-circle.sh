144a29e54ffd721e46f8cc2d8f07f043013a8fd5
Paul Carduner <paul@carduner.net>
2017-03-23 12:46:31 -0700

    switch to using extracted js-interpter-tyrant test runner

--- tests/test-on-circle.sh
+++ tests/test-on-circle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-tests/build-test-report.js --threads 1 -run --diff --verbose --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX
+`npm bin`/js-interpreter-tyrant --threads 1 -run --diff --verbose --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX
 t1=$?
-mv tests/test-results-new.json $CIRCLE_ARTIFACTS
+mv tyrant/test-results-new.json $CIRCLE_ARTIFACTS
 exit $t1
