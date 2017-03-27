7712aeec36e100fac29409820212a4a3508b8424
Paul Carduner <paul@carduner.net>
2017-03-27 12:50:50 -0700

    Pass js-interpreter host path in directly to js-interpreter-tyrant

--- tests/test-on-circle.sh
+++ tests/test-on-circle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 -run --diff --verbose --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX
+./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 -run --diff --verbose --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX --hostPath bin/run.js
 t1=$?
 mv tyrant/test-results-new.json $CIRCLE_ARTIFACTS
 exit $t1
