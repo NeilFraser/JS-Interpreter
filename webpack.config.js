ceb3eba9dd13846b97e2fce7140a982a65e4f370
Amin Marashi <marashi.s.amin@gmail.com>
2017-03-29 11:41:36 +0800

    Add es2015 preset to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: 'babel-loader?presets[]=es2015',
             },
         ],
     },
