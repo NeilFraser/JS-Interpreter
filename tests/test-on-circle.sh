54c97df6c00e7a3ae07b258409619a2fa7f293c3
Paul Carduner <paul@carduner.net>
2017-04-27 15:03:47 -0700

    Switch to new version of test runner, and rerun failed tests once in circle

--- tests/test-on-circle.sh
+++ tests/test-on-circle.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 git clone https://github.com/tc39/test262.git --depth 1 tyrant/test262
-./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 -run --diff --verbose --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX --hostPath bin/run.js
+./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 --run --diff --verbose --splitInto $CIRCLE_NODE_TOTAL --splitIndex $CIRCLE_NODE_INDEX --hostPath bin/run.js || \
+    ./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 --rerun --verbose --hostPath bin/run.js
 t1=$?
 mv tyrant/test-results-new.json $CIRCLE_ARTIFACTS
 exit $t1
