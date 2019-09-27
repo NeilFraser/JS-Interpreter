1c73b8c81dd70d510dd6a494b7b50cf608366a17
Neil Fraser <fraser@google.com>
2019-09-26 22:24:24 -0700

    Fix get/set in object literal.
    Resolves issue #162.

--- interpreter.js
+++ interpreter.js
@@ -3647,7 +3647,8 @@ Interpreter.prototype['stepObjectExpression'] = function(stack, state, node) {
         get: kinds['get'],
         set: kinds['set']
       };
-      this.setProperty(state.object_, key, null, descriptor);
+      this.setProperty(state.object_, key, Interpreter.VALUE_IN_DESCRIPTOR,
+                       descriptor);
     } else {
       // Set a normal property with a value.
       this.setProperty(state.object_, key, kinds['init']);
