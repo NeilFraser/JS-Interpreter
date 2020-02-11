5aec279ab65f998340405347d2af4f56daa1f44f
Amin Marashi <marashi.s.amin@gmail.com>
2020-02-11 11:17:01 +0800

    Separate production and development build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path    = require('path');
 const webpack = require('webpack');
 
-module.exports = [
-    {
-        mode  : 'production',
+function interpreterConfig(
+    mode     = 'development',
+    filename = 'js-interpreter.js',
+    devtool = false
+) {
+    return {
+        mode,
         entry : path.resolve(__dirname, 'src', 'js-interpreter.js'),
         output: {
-            filename     : 'js-interpreter.js',
+            filename,
             globalObject : 'this',
             library      : 'JSInterpreter',
             libraryExport: 'default',
@@ -15,26 +19,42 @@ module.exports = [
         },
         plugins: [
             new webpack.ProvidePlugin({
-                'acorn': path.resolve(__dirname, 'original-repo', 'acorn.js')
+                acorn: path.resolve(__dirname, 'original-repo', 'acorn.js')
             })
         ],
-    },
-    {
-        mode  : 'production',
+        devtool: devtool ? 'cheap-source-map' : false,
+    };
+}
+
+function cliConfig(
+    mode     = 'development',
+    filename = 'js-interpreter.js',
+    devtool = false
+) {
+    return {
+        mode,
         entry : path.resolve(__dirname, 'src', 'cli.js'),
         target: 'node',
         output: {
-            filename     : 'cli.js',
-            path         : path.resolve(__dirname, 'lib'),
+            filename,
+            path    : path.resolve(__dirname, 'lib'),
         },
         plugins: [
             new webpack.ProvidePlugin({
-                'acorn': path.resolve(__dirname, 'original-repo', 'acorn.js')
+                acorn: path.resolve(__dirname, 'original-repo', 'acorn.js')
             }),
             new webpack.BannerPlugin({
                 banner: '#!/usr/bin/env node',
                 raw   : true,
             }),
         ],
-    },
+        devtool: devtool ? 'cheap-source-map' : false,
+    };
+}
+
+module.exports = [
+    interpreterConfig('development', 'js-interpreter.js', false),
+    interpreterConfig('production', 'js-interpreter.min.js', true),
+    cliConfig('development', 'cli.js', false),
+    cliConfig('production', 'cli.min.js', true),
 ];
