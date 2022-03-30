41e801004fd7c49055760dfcad3cc235dff6d6df
Neil Fraser <fraser@google.com>
2022-03-30 15:13:55 +0200

    Decompose (de)serialize functions.
    The ‘serialize/deserialize’ API remains the same.
    
    Also now throws explicit error if interperter is compiled (previously it would throw a hard to identify error).
    
    Also disable the deserialize button if the serialization textarea is blank (one less button that the user can push inappropriately).

--- demos/serialize.js
+++ demos/serialize.js
@@ -16,8 +16,26 @@ var NODE_CONSTRUCTOR;
 var NODE_LOC_CONSTRUCTOR;
 var LINE_LOC_CONSTRUCTOR;
 
-function recordAcornConstructons(interpreter) {
+var LOC_REGEX = /^(\d*):(\d*)-(\d*):(\d*) ?(.*)$/;
+
+/**
+ * All non-primitives in the interpreter.
+ * @type {!Array<!Object>}
+ */
+ var objectList = [];
+
+/**
+ * Inspect an interpreter and record the constructors used to create new nodes.
+ * @param {!Interpreter} interpreter JS-Interpreter instance.
+ * @private
+ */
+function recordAcornConstructons_(interpreter) {
   // Constructors for objects within Acorn.
+  if (!interpreter.ast) {
+    // The 'ast' property has been renamed by the compiler.
+    throw Error('Cannot serialize/deserialize compressed JS-Interpreter. ' +
+        'Use acorn.js and interpreter.js instead of acorn_interpreter.js')
+  }
   NODE_CONSTRUCTOR = interpreter.ast.constructor;
   NODE_LOC_CONSTRUCTOR = interpreter.ast.loc &&
       interpreter.ast.loc.constructor;
@@ -25,31 +43,12 @@ function recordAcornConstructons(interpreter) {
       interpreter.ast.loc.end.constructor;
 }
 
+/**
+ * Populate an interpreter to the state defined by serialized JSON.
+ * @param {string} json Serialized JSON.
+ * @param {!Interpreter} interpreter JS-Interpreter instance to restore to.
+ */
 function deserialize(json, interpreter) {
-  function decodeValue(value) {
-    if (value && typeof value === 'object') {
-      var data;
-      if ((data = value['#'])) {
-       // Object reference: {'#': 42}
-       value = objectList[data];
-        if (!value) {
-          throw ReferenceError('Object reference not found: ' + data);
-        }
-        return value;
-      }
-      if ((data = value['Number'])) {
-        // Special number: {'Number': 'Infinity'}
-        return Number(data);
-      }
-      if ((data = value['Value'])) {
-        // Special value: {'Value': 'undefined'}
-        if (value['Value'] === 'undefined') {
-          return undefined;
-        }
-      }
-    }
-    return value;
-  }
   var stack = interpreter.getStateStack();
   if (!Array.isArray(json)) {
     throw TypeError('Top-level JSON is not a list.');
@@ -58,172 +57,195 @@ function deserialize(json, interpreter) {
     // Require native functions to be present.
     throw Error('Interpreter must be initialized prior to deserialization.');
   }
-  recordAcornConstructons(interpreter);
-  var LOC_REGEX = /^(\d*):(\d*)-(\d*):(\d*) ?(.*)$/;
+  recordAcornConstructons_(interpreter);
   // Find all native functions in existing interpreter.
-  var objectList = [];
-  objectHunt_(stack, objectList);
-  var functionHash = Object.create(null);
+  objectList = [];
+  objectHunt_(stack);
+  var functionMap = Object.create(null);
   for (var i = 0; i < objectList.length; i++) {
-    if (typeof objectList[i] === 'function') {
-      functionHash[objectList[i].id] = objectList[i];
+    var obj = objectList[i];
+    if (typeof obj === 'function') {
+      functionMap[obj.id] = obj;
     }
   }
   // First pass: Create object stubs for every object.
-  objectList = [];
+  objectList = new Array(json.length);
   for (var i = 0; i < json.length; i++) {
-    var jsonObj = json[i];
-    var obj;
-    switch (jsonObj['type']) {
-      case 'Map':
-        obj = Object.create(null);
-        break;
-      case 'Object':
-        obj = {};
-        break;
-      case 'ScopeReference':
-        obj = Interpreter.SCOPE_REFERENCE;
-        break;
-      case 'Function':
-        obj = functionHash[jsonObj['id']];
-        if (!obj) {
-          throw RangeError('Function ID not found: ' + jsonObj['id']);
-        }
-        break;
-      case 'Array':
-        // Currently we assume that Arrays are not sparse.
-        obj = [];
-        break;
-      case 'Date':
-        obj = new Date(jsonObj['data']);
-        if (isNaN(obj)) {
-          throw TypeError('Invalid date: ' + jsonObj['data']);
-        }
-        break;
-      case 'RegExp':
-        obj = RegExp(jsonObj['source'], jsonObj['flags']);
-        break;
-      case 'PseudoObject':
-        obj = new Interpreter.Object(null);
-        break;
-      case 'Scope':
-        obj = new Interpreter.Scope(undefined, undefined, undefined);
-        break;
-      case 'State':
-        obj = new Interpreter.State(undefined, undefined);
-        break;
-      case 'Node':
-        obj = new NODE_CONSTRUCTOR();
-        delete obj.start;
-        delete obj.end;
-        var locText = jsonObj['loc'];
-        if (locText) {
-          var loc = new NODE_LOC_CONSTRUCTOR();
-          var m = locText.match(LOC_REGEX);
-          var locStart = null;
-          if (m[1] || m[2]) {
-            locStart = new LINE_LOC_CONSTRUCTOR();
-            locStart.line = Number(m[1]);
-            locStart.column = Number(m[2]);
-          }
-          loc.start = locStart;
-          var locEnd = null;
-          if (m[3] || m[4]) {
-            locEnd = new LINE_LOC_CONSTRUCTOR();
-            locEnd.line = Number(m[3]);
-            locEnd.column = Number(m[4]);
-          }
-          loc.end = locEnd;
-          if (m[5]) {
-            loc.source = decodeURI(m[5]);
-          } else {
-            delete loc.source;
-          }
-          obj.loc = loc;
-        }
-        break;
-      default:
-        throw TypeError('Unknown type: ' + jsonObj['type']);
-    }
-    objectList[i] = obj;
+    objectList[i] = createObjectStub_(json[i], functionMap);
   }
   // Second pass: Populate properties for every object.
   for (var i = 0; i < json.length; i++) {
-    var jsonObj = json[i];
-    var obj = objectList[i];
-    // Repopulate objects.
-    var props = jsonObj['props'];
-    if (props) {
-      var nonConfigurable = jsonObj['nonConfigurable'] || [];
-      var nonEnumerable = jsonObj['nonEnumerable'] || [];
-      var nonWritable = jsonObj['nonWritable'] || [];
-      var getter = jsonObj['getter'] || [];
-      var setter = jsonObj['setter'] || [];
-      var names = Object.getOwnPropertyNames(props);
-      for (var j = 0; j < names.length; j++) {
-        var name = names[j];
-        var descriptor = {
-          configurable: nonConfigurable.indexOf(name) === -1,
-          enumerable: nonEnumerable.indexOf(name) === -1
-        };
-        var hasGetter = getter.indexOf(name) !== -1;
-        var hasSetter = setter.indexOf(name) !== -1;
-        if (hasGetter || hasSetter) {
-          if (hasGetter) {
-            descriptor.get = interpreter.setProperty.placeholderGet_;
-          }
-          if (hasSetter) {
-            descriptor.set = interpreter.setProperty.placeholderSet_;
-          }
-        } else {
-          descriptor.value = decodeValue(props[name]);
-          descriptor.writable = nonWritable.indexOf(name) === -1;
-        }
-        Object.defineProperty(obj, name, descriptor);
-      }
-    }
-    // Repopulate arrays.
-    if (Array.isArray(obj)) {
-      var data = jsonObj['data'];
-      if (data) {
-        for (var j = 0; j < data.length; j++) {
-          obj.push(decodeValue(data[j]));
-        }
-      }
-    }
+    populateObject_(json[i], objectList[i]);
   }
   // First object is the interpreter.
   var root = objectList[0];
   for (var prop in root) {
     interpreter[prop] = root[prop];
   }
+  objectList = [];  // Garbage collect.
 }
 
-function serialize(interpreter) {
-  function encodeValue(value) {
-    if (value && (typeof value === 'object' || typeof value === 'function')) {
-      var ref = objectList.indexOf(value);
-      if (ref === -1) {
-        throw RangeError('Object not found in table.');
+/**
+ * Given the JSON description of an object, create the object.
+ * But don't populate its properties yet.
+ * @param {!Object} jsonObj JSON description of object.
+ * @param {!Object} functionMap Map of ID to native functions.
+ * @return {!Object} Stub of real object.
+ * @private
+ */
+ function createObjectStub_(jsonObj, functionMap) {
+  switch (jsonObj['type']) {
+    case 'Map':
+      return Object.create(null);
+    case 'Object':
+      return {};
+    case 'ScopeReference':
+      return Interpreter.SCOPE_REFERENCE;
+    case 'Function':
+      var obj = functionMap[jsonObj['id']];
+      if (!obj) {
+        throw RangeError('Function ID not found: ' + jsonObj['id']);
       }
-      return {'#': ref};
-    }
-    if (value === undefined) {
-      return {'Value': 'undefined'};
-    }
-    if (typeof value === 'number') {
-      if (value === Infinity) {
-        return {'Number': 'Infinity'};
-      } else if (value === -Infinity) {
-        return {'Number': '-Infinity'};
-      } else if (isNaN(value)) {
-        return {'Number': 'NaN'};
-      } else if (1 / value === -Infinity) {
-        return {'Number': '-0'};
+      return obj;
+    case 'Array':
+      // Currently we assume that Arrays are not sparse.
+      return [];
+    case 'Date':
+      var obj = new Date(jsonObj['data']);
+      if (isNaN(obj)) {
+        throw TypeError('Invalid date: ' + jsonObj['data']);
       }
-    }
-    return value;
+      return obj;
+    case 'RegExp':
+      return RegExp(jsonObj['source'], jsonObj['flags']);
+    case 'PseudoObject':
+      return new Interpreter.Object(null);
+    case 'Scope':
+      return new Interpreter.Scope(undefined, undefined, undefined);
+    case 'State':
+      return new Interpreter.State(undefined, undefined);
+    case 'Node':
+      var obj = new NODE_CONSTRUCTOR();
+      delete obj.start;
+      delete obj.end;
+      var locText = jsonObj['loc'];
+      if (locText) {
+        var loc = new NODE_LOC_CONSTRUCTOR();
+        var m = locText.match(LOC_REGEX);
+        var locStart = null;
+        if (m[1] || m[2]) {
+          locStart = new LINE_LOC_CONSTRUCTOR();
+          locStart.line = Number(m[1]);
+          locStart.column = Number(m[2]);
+        }
+        loc.start = locStart;
+        var locEnd = null;
+        if (m[3] || m[4]) {
+          locEnd = new LINE_LOC_CONSTRUCTOR();
+          locEnd.line = Number(m[3]);
+          locEnd.column = Number(m[4]);
+        }
+        loc.end = locEnd;
+        if (m[5]) {
+          loc.source = decodeURI(m[5]);
+        } else {
+          delete loc.source;
+        }
+        obj.loc = loc;
+      }
+      return obj;
   }
+  throw TypeError('Unknown type: ' + jsonObj['type']);
+}
+
+/**
+ * Repopulate an object's or array's properties based on th JSON description.
+ * @param {!Object} jsonObj JSON object describing property contents.
+ * @param {!Object} obj Object to set properties on.
+ * @private
+ */
+function populateObject_(jsonObj, obj) {
+  // Repopulate objects.
+  var props = jsonObj['props'];
+  if (props) {
+    var nonConfigurable = jsonObj['nonConfigurable'] || [];
+    var nonEnumerable = jsonObj['nonEnumerable'] || [];
+    var nonWritable = jsonObj['nonWritable'] || [];
+    var getter = jsonObj['getter'] || [];
+    var setter = jsonObj['setter'] || [];
+    var names = Object.getOwnPropertyNames(props);
+    for (var j = 0; j < names.length; j++) {
+      var name = names[j];
+      var descriptor = {
+        configurable: nonConfigurable.indexOf(name) === -1,
+        enumerable: nonEnumerable.indexOf(name) === -1
+      };
+      var hasGetter = getter.indexOf(name) !== -1;
+      var hasSetter = setter.indexOf(name) !== -1;
+      if (hasGetter || hasSetter) {
+        if (hasGetter) {
+          descriptor.get = interpreter.setProperty.placeholderGet_;
+        }
+        if (hasSetter) {
+          descriptor.set = interpreter.setProperty.placeholderSet_;
+        }
+      } else {
+        descriptor.value = decodeValue_(props[name]);
+        descriptor.writable = nonWritable.indexOf(name) === -1;
+      }
+      Object.defineProperty(obj, name, descriptor);
+    }
+  }
+  // Repopulate arrays.
+  if (Array.isArray(obj)) {
+    var data = jsonObj['data'];
+    if (data) {
+      for (var j = 0; j < data.length; j++) {
+        obj.push(decodeValue_(data[j]));
+      }
+    }
+  }
+}
+
+/**
+ * Given a serialized value, decode it to a real value.
+ * Most values are themselves.  But objects are references, and Infinity, NaN,
+ * -0 and undefined are specially encoded.
+ * @param {*} value Serialized value.
+ * @return {*} Real value.
+ * @private
+ */
+ function decodeValue_(value) {
+  if (value && typeof value === 'object') {
+    var data;
+    if ((data = value['#'])) {
+      // Object reference: {'#': 42}
+      value = objectList[data];
+      if (!value) {
+        throw ReferenceError('Object reference not found: ' + data);
+      }
+      return value;
+    }
+    if ((data = value['Number'])) {
+      // Special number: {'Number': 'Infinity'}
+      return Number(data);
+    }
+    if ((data = value['Value'])) {
+      // Special value: {'Value': 'undefined'}
+      if (value['Value'] === 'undefined') {
+        return undefined;
+      }
+    }
+  }
+  return value;
+}
+
+/**
+ * Generate JSON that completely describes an interpreter's state.
+ * @param {!Interpreter} interpreter JS-Interpreter instance to serialize.
+ * @return {string} Serialized JSON.
+ */
+function serialize(interpreter) {
   // Shallow-copy all properties of interest onto a root object.
   var properties = [
     'OBJECT', 'OBJECT_PROTO',
@@ -250,9 +272,9 @@ function serialize(interpreter) {
     root[properties[i]] = interpreter[properties[i]];
   }
 
-  recordAcornConstructons(interpreter);
+  recordAcornConstructons_(interpreter);
   // Find all objects.
-  var objectList = [];
+  objectList = [];
   objectHunt_(root, objectList);
   // Serialize every object.
   var json = [];
@@ -285,7 +307,7 @@ function serialize(interpreter) {
         // Currently we assume that Arrays are not sparse.
         jsonObj['type'] = 'Array';
         if (obj.length) {
-          jsonObj['data'] = obj.map(encodeValue);
+          jsonObj['data'] = obj.map(encodeValue_);
         }
         continue;  // No need to index properties.
       case Date.prototype:
@@ -342,7 +364,7 @@ function serialize(interpreter) {
         jsonObj['loc'] = locText;
       } else {
         var descriptor = Object.getOwnPropertyDescriptor(obj, name);
-        props[name] = encodeValue(descriptor.value);
+        props[name] = encodeValue_(descriptor.value);
         if (!descriptor.configurable) {
           nonConfigurable.push(name);
         }
@@ -379,32 +401,73 @@ function serialize(interpreter) {
       jsonObj['setter'] = setter;
     }
   }
+  objectList = [];  // Garbage collect.
   return json;
 }
 
-// Recursively search the stack to find all non-primitives.
-function objectHunt_(node, objectList) {
+/**
+ * Given a real value, encode it to a serialized value.
+ * Most values are themselves.  But objects are references, and Infinity, NaN,
+ * -0 and undefined are specially encoded.
+ * @param {*} value Real value.
+ * @return {*} Serialized value.
+ */
+ function encodeValue_(value) {
+  if (value && (typeof value === 'object' || typeof value === 'function')) {
+    var ref = objectList.indexOf(value);
+    if (ref === -1) {
+      throw RangeError('Object not found in table.');
+    }
+    return {'#': ref};
+  }
+  if (value === undefined) {
+    return {'Value': 'undefined'};
+  }
+  if (typeof value === 'number') {
+    if (value === Infinity) {
+      return {'Number': 'Infinity'};
+    } else if (value === -Infinity) {
+      return {'Number': '-Infinity'};
+    } else if (isNaN(value)) {
+      return {'Number': 'NaN'};
+    } else if (1 / value === -Infinity) {
+      return {'Number': '-0'};
+    }
+  }
+  return value;
+}
+
+/**
+ * Recursively search the stack to find all non-primitives.
+ * Stores the results in the objectList global variable.
+ * @param {!Object} node Root node to start searching.
+ */
+function objectHunt_(node) {
   if (node && (typeof node === 'object' || typeof node === 'function')) {
     if (objectList.indexOf(node) !== -1) {
       return;
     }
     objectList.push(node);
     if (typeof node === 'object') {  // Recurse.
-      var isAcornNode = Object.getPrototypeOf(node) === NODE_CONSTRUCTOR.prototype;
+      var isAcornNode =
+          Object.getPrototypeOf(node) === NODE_CONSTRUCTOR.prototype;
       var names = Object.getOwnPropertyNames(node);
       for (var i = 0; i < names.length; i++) {
-        if (isAcornNode && names[i] === 'loc') {
+        var name = names[i];
+        if (isAcornNode && name === 'loc') {
           continue;  // Skip over node locations, they are specially handled.
         }
         try {
-          objectHunt_(node[names[i]], objectList);
+          var nextNode = node[name];
         } catch (e) {
           // Accessing some properties may trigger a placeholder getter.
           // Squelch this error, but re-throw any others.
           if (e.message !== 'Placeholder getter') {
             throw e;
           }
+          continue;
         }
+        objectHunt_(nextNode);
       }
     }
   }
