c71bfbd8d7328441fe0a1b2a61b7be144ed05310
Neil Fraser <fraser@google.com>
2022-12-05 23:32:16 +0100

    Bump Acorn from 0.4.1 to 0.5.0
    Seven years ago I'd determined that 0.4.1 was the very last version that would work with Closure Compiler.  However, this no longer seems to be the case, 0.5.0 (12 March 2014) works fine now.

--- acorn.js
+++ acorn.js
@@ -27,7 +27,7 @@
 })(this, function(exports) {
   "use strict";
 
-  exports.version = "0.4.1";
+  exports.version = "0.5.0";
   // Plus additional edits marked with 'JS-Interpreter change' comments.
 
   // The main exported interface (under `self.acorn` when in the
@@ -63,8 +63,13 @@
     // trailing commas in array and object literals.
     allowTrailingCommas: true,
     // By default, reserved words are not enforced. Enable
-    // `forbidReserved` to enforce them.
+    // `forbidReserved` to enforce them. When this option has the
+    // value "everywhere", reserved words and keywords can also not be
+    // used as property names.
     forbidReserved: false,
+    // When enabled, a return at the top level is not considered an
+    // error.
+    allowReturnOutsideFunction: false,
     // When `locations` is on, `loc` properties holding objects with
     // `start` and `end` properties in `{line, column}` form (with
     // line being 1-based and column 0-based) will be attached to the
@@ -78,7 +83,8 @@
     // character offsets that denote the start and end of the comment.
     // When the `locations` option is on, two more parameters are
     // passed, the full `{line, column}` locations of the start and
-    // end of the comments.
+    // end of the comments. Note that you are not allowed to call the
+    // parser from the callback—that will corrupt its internal state.
     onComment: null,
     // Nodes have their start and end characters offsets recorded in
     // `start` and `end` properties (directly on the node, rather than
@@ -95,11 +101,11 @@
     // toplevel forms of the parsed file to the `Program` (top) node
     // of an existing parse tree.
     program: null,
-    // When `location` is on, you can pass this to record the source
+    // When `locations` is on, you can pass this to record the source
     // file in every node's `loc` object.
     sourceFile: null,
     // This value, if given, is stored in every node, whether
-    // `location` is on or off.
+    // `locations` is on or off.
     directSourceFile: null
   };
 
@@ -142,6 +148,7 @@
 
     var t = {};
     function getToken(forceRegexp) {
+      lastEnd = tokEnd;
       readToken(forceRegexp);
       t.start = tokStart; t.end = tokEnd;
       t.startLoc = tokStartLoc; t.endLoc = tokEndLoc;
@@ -743,7 +750,13 @@
     // -- Neil Fraser, December 2022.
     // https://github.com/acornjs/acorn/issues/1163
     if (mods && !/^[gmi]*$/.test(mods)) raise(start, "Invalid regexp flag");
-    return finishToken(_regexp, new RegExp(content, mods));
+    try {
+      var value = new RegExp(content, mods);
+    } catch (e) {
+      if (e instanceof SyntaxError) raise(start, e.message);
+      raise(e);
+    }
+    return finishToken(_regexp, value);
   }
 
   // Read an integer in the given radix. Return null if zero digits
@@ -906,13 +919,8 @@
   function readWord() {
     var word = readWord1();
     var type = _name;
-    if (!containsEsc) {
-      if (isKeyword(word)) type = keywordTypes[word];
-      else if (options.forbidReserved &&
-               (options.ecmaVersion === 3 ? isReservedWord3 : isReservedWord5)(word) ||
-               strict && isStrictReservedWord(word))
-        raise(tokStart, "The keyword '" + word + "' is reserved");
-    }
+    if (!containsEsc && isKeyword(word))
+      type = keywordTypes[word];
     return finishToken(type, word);
   }
 
@@ -952,7 +960,7 @@
 
   function setStrict(strct) {
     strict = strct;
-    tokPos = lastEnd;
+    tokPos = tokStart;
     if (options.locations) {
       while (tokPos < tokLineStart) {
         tokLineStart = input.lastIndexOf("\n", tokLineStart - 2) + 1;
@@ -1192,7 +1200,8 @@
       return finishNode(node, "IfStatement");
 
     case _return:
-      if (!inFunction) raise(tokStart, "'return' outside of function");
+      if (!inFunction && !options.allowReturnOutsideFunction)
+        raise(tokStart, "'return' outside of function");
       next();
 
       // In `return` (and `break`/`continue`), the keywords with
@@ -1727,7 +1736,20 @@
 
   function parseIdent(liberal) {
     var node = startNode();
-    node.name = tokType === _name ? tokVal : (liberal && !options.forbidReserved && tokType.keyword) || unexpected();
+    if (liberal && options.forbidReserved == "everywhere") liberal = false;
+    if (tokType === _name) {
+      if (!liberal &&
+          (options.forbidReserved &&
+           (options.ecmaVersion === 3 ? isReservedWord3 : isReservedWord5)(tokVal) ||
+           strict && isStrictReservedWord(tokVal)) &&
+          input.slice(tokStart, tokEnd).indexOf("\\") == -1)
+        raise(tokStart, "The keyword '" + tokVal + "' is reserved");
+      node.name = tokVal;
+    } else if (liberal && tokType.keyword) {
+      node.name = tokType.keyword;
+    } else {
+      unexpected();
+    }
     tokRegexpAllowed = false;
     next();
     return finishNode(node, "Identifier");
