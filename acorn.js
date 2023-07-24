926fcb585035f96e2ca25da1bf46332ac02dc62c
Neil Fraser <fraser@google.com>
2023-07-24 20:46:55 +0200

    Fix null vs undefined type.

--- acorn.js
+++ acorn.js
@@ -957,7 +957,7 @@
    */
   function readInt(radix, len) {
     var start = tokPos, total = 0;
-    for (var i = 0, e = len == null ? Infinity : len; i < e; ++i) {
+    for (var i = 0, e = (len === undefined) ? Infinity : len; i < e; ++i) {
       var code = input.charCodeAt(tokPos), val;
       if (code >= 97) val = code - 97 + 10; // a
       else if (code >= 65) val = code - 65 + 10; // A
@@ -967,8 +967,9 @@
       ++tokPos;
       total = total * radix + val;
     }
-    if (tokPos === start || len !== null && tokPos - start !== len) return null;
-
+    if (tokPos === start || len !== undefined && tokPos - start !== len) {
+      return null;
+    }
     return total;
   }
 
